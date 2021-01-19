7f703d3869907cc4429abc0687f0e25a5c5136ba
Donz2020 <donoval.candolfi@studenti.unicam.it>
2021-01-19 22:54:30 +0100

    fix folder

--- mqtt-php/lib/phpMQTT_Extended.php
+++ mqtt-php/lib/phpMQTT_Extended.php
@@ -25,7 +25,7 @@ class phpMQTT_Extended extends phpMQTT
         echo date('r: ') . $message . PHP_EOL;
         $var = date('r: ') . $message . PHP_EOL;
         $filename = $this->logfile;
-        $myfile = fopen("C:/Users/stage/Downloads/mqtt-php/log/" . $filename, "a") or print("Unable to open file!");
+        $myfile = fopen("D:/progetti_stage/mqtt-php/log" . $filename, "a") or print("Unable to open file!");
         fwrite($myfile, $var);
         fclose($myfile);
     }
